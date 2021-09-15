8c7dfd370b12d1b817a0454d4146d694a747a0b9
Mcswiggi <mrchitrason@gmail.com>
2021-09-14 23:18:40 -0400

    add style to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,9 @@ const Navbar = () => {
           <div className={classes.grow}/>
           <div className={classes.button}/>
             <IconButton aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={2}></Badge>
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart/>
+              </Badge>
             </IconButton>
         </Toolbar>
 
